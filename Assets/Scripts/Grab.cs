497b3f16c80423ee64acc75ad3cefe854b4f7382
Seth Johnson <joh08230@umn.edu>
2018-12-14 14:15:09 -0600

    can move and bounce cube

--- Assets/Scripts/Grab.cs
+++ Assets/Scripts/Grab.cs
@@ -16,18 +16,20 @@ public class Grab : MonoBehaviour
   private bool startedGrabbing = false;
   private bool isGrabbing = false;
   private bool finishedGrabbing = false;
+  private GameObject inHand;
 
   // Start is called before the first frame update
   void Start()
   {
     trackedObj = GetComponent<SteamVR_TrackedObject>();
-  }
+  }  
 
   //Update is called once per frame
   void Update()
   {
     if (Controller.GetHairTriggerDown() && !isGrabbing)
     {
+      Debug.Log("Begin grabbing");
       startedGrabbing = true;
       isGrabbing = true;
       finishedGrabbing = false;
@@ -35,6 +37,8 @@ public class Grab : MonoBehaviour
     }
     if (Controller.GetHairTriggerUp() && isGrabbing)
     {
+      Debug.Log("Stahp grabbing");
+
       startedGrabbing = false;
       isGrabbing = false;
       finishedGrabbing = true;
@@ -44,26 +48,29 @@ public class Grab : MonoBehaviour
 
   void MoveObjectInHand()
   {
-    // grab the public GObj from the Selector script
-    GameObject inHand = sphereSelector.GetComponent<Selector>().collidingObject;
-
-    if (inHand == null)
+     if (finishedGrabbing && inHand)
     {
-      return;
-    }
-
-    if (startedGrabbing)
-    {
-      // parent the collidingObject to the sphereSelector
-      if (inHand.transform.parent != sphereSelector.transform) oldParent = inHand.transform.parent;
-      Debug.Log(oldParent);
-      inHand.transform.SetParent(sphereSelector.gameObject.transform);
-    }
-    else if (finishedGrabbing)
-    {
-      // un-parent the collidingObject from the sphereSelector
+      // un-parent inHand from the sphereSelector
       inHand.transform.SetParent(oldParent);
       Debug.Log("Resetting parent " + oldParent);
+
+      // set inHand's is kinematic back to false --> "turn back on" physics
+      inHand.GetComponentInChildren<Rigidbody>().isKinematic = false;
+      inHand = null;
     }
+    else if (startedGrabbing)
+    {
+      // grab the public GObj from the Selector script
+      inHand = sphereSelector.GetComponent<Selector>().collidingObject;
+
+      // parent inHand to the sphereSelector
+      if (inHand.transform.parent != sphereSelector.transform) oldParent = inHand.transform.parent;
+      // Debug.Log(oldParent);
+      inHand.transform.SetParent(sphereSelector.gameObject.transform);
+
+      // set inHand's is kinematic to true --> "turn off" physics
+      inHand.GetComponentInChildren<Rigidbody>().isKinematic = true;
+    }
+
   }
 }
