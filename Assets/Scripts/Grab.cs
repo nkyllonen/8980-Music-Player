ae10aab78c9c7d18d4d52a850b3ea79802c326af
Seth Johnson <joh08230@umn.edu>
2018-12-15 11:35:32 -0600

    Shake detects when right and left spheres have same inHand

--- Assets/Scripts/Grab.cs
+++ Assets/Scripts/Grab.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Grab : MonoBehaviour
 {
-  public GameObject sphereSelector;
+  public GameObject selectionSphere;
 
   private Transform oldParent;
   private SteamVR_TrackedObject trackedObj;
@@ -16,7 +16,7 @@ public class Grab : MonoBehaviour
   private bool startedGrabbing = false;
   private bool isGrabbing = false;
   private bool finishedGrabbing = false;
-  private GameObject inHand;
+  public GameObject inHand;
 
   // Start is called before the first frame update
   void Start()
@@ -50,7 +50,7 @@ public class Grab : MonoBehaviour
   {
      if (finishedGrabbing && inHand)
     {
-      // un-parent inHand from the sphereSelector
+      // un-parent inHand from the selectionSphere
       inHand.transform.SetParent(oldParent);
       Debug.Log("Resetting parent " + oldParent);
 
@@ -61,12 +61,12 @@ public class Grab : MonoBehaviour
     else if (startedGrabbing)
     {
       // grab the public GObj from the Selector script
-      inHand = sphereSelector.GetComponent<Selector>().collidingObject;
+      inHand = selectionSphere.GetComponent<Selector>().collidingObject;
 
-      // parent inHand to the sphereSelector
-      if (inHand.transform.parent != sphereSelector.transform) oldParent = inHand.transform.parent;
+      // parent inHand to the selectionSphere
+      if (inHand.transform.parent != selectionSphere.transform) oldParent = inHand.transform.parent;
       // Debug.Log(oldParent);
-      inHand.transform.SetParent(sphereSelector.gameObject.transform);
+      inHand.transform.SetParent(selectionSphere.gameObject.transform);
 
       // set inHand's is kinematic to true --> "turn off" physics
       inHand.GetComponentInChildren<Rigidbody>().isKinematic = true;
