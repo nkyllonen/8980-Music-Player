91d978ce8cb733ac034351c29ba42410c7c4f602
Seth Johnson <joh08230@umn.edu>
2018-12-15 12:39:02 -0600

    beginning basic spawning from parsing

---
+++ Assets/Scripts/ParseCSV.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Songs data object class
+public class Song
+{
+	public string title, album_name, filepath;
+
+	public Song()
+	{
+		title = "";
+		album_name = "";
+		filepath = "";
+	}
+
+	public Song(string name, string album, string file)
+	{
+		title = name;
+		album_name = album;
+		filepath = file;
+	}
+}
+
+public class ParseCSV : MonoBehaviour
+{
+	private string filepath = "";
+
+	// Use this for initialization
+	void Start ()
+	{
+		// TODO: verify filepath is valid?
+	}
+	
+	// Update is called once per frame
+	void Update () {}
+
+	public List<Song> Parse()
+	{
+		// name the List with the artist name
+		List<Song> BTS = new List<Song>();
+
+		BTS.Add(new Song("Butterfly", "Young Forever", "filepath"));
+		BTS.Add(new Song("SpineBreaker", "School Luv Affair", "filepath"));
+		BTS.Add(new Song("Maze", "Love Yourself: Tear", "somefile"));
+
+		return BTS;
+	}
+}
---
+++ Assets/Scripts/SetupScene.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetupScene : MonoBehaviour
+{
+	public Mesh crateMesh;
+	
+	private ParseCSV parseCSV;
+
+	// TODO: make this a List of artists, not one artist
+	public List<Song> BTS;
+
+	// Use this for initialization
+	void Start ()
+	{
+		// grab the parse script + call Parse
+		parseCSV = this.GetComponent<ParseCSV>();
+		BTS = parseCSV.Parse();
+	}
+	
+	// Update is called once per frame
+	void Update () { }
+}
