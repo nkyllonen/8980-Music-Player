6e697eea14b716ea5ba9057950d8a7eabb3750e3
Nikki N Kyllonen <kyllo089@ad.umn.edu>
2018-12-18 13:00:20 -0600

    displays numbered playlist items + room touch up

--- Assets/Scripts/PlaylistBuilder.cs
+++ Assets/Scripts/PlaylistBuilder.cs
@@ -6,6 +6,8 @@ public class PlaylistBuilder : MonoBehaviour {
 	// HashSet of unique Song objects
 	public HashSet<Song> playlist;
 
+    public TextMesh playlist_info;
+
 	private Color selection_color = Color.green;
 	private Color old_color;
 
@@ -19,7 +21,15 @@ public class PlaylistBuilder : MonoBehaviour {
 	// Update is called once per frame
 	void Update ()
 	{
-		
+        // display playlist information in the textmesh
+        playlist_info.text = "";
+        int i = 0;
+
+        foreach (Song s in playlist)
+        {
+            playlist_info.text += (i+1) + ". " + s.title + " by " + s.artist_name + "\n";
+            i++;
+        }
 	}
 
 	void OnTriggerEnter(Collider other)
