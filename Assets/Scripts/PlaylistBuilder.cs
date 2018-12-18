48d44ecf5cfe8aebed38106dd81cdfe422c11424
Seth Johnson <joh08230@umn.edu>
2018-12-18 14:50:16 -0600

    playlist shuffling working with playlist crate

--- Assets/Scripts/PlaylistBuilder.cs
+++ Assets/Scripts/PlaylistBuilder.cs
@@ -8,13 +8,16 @@ public class PlaylistBuilder : MonoBehaviour {
 
     public TextMesh playlist_info;
 
+	public GameObject playlist_crate;
+
 	private Color selection_color = Color.green;
 	private Color old_color;
 
 	// Use this for initialization
 	void Start ()
 	{
-		playlist = new HashSet<Song>();
+		// refer to Playlist's HashSet
+		playlist = playlist_crate.GetComponent<Playlist>().songs;
 		old_color = this.GetComponent<Renderer>().material.color;
 	}
 	
@@ -22,12 +25,13 @@ public class PlaylistBuilder : MonoBehaviour {
 	void Update ()
 	{
         // display playlist information in the textmesh
+		playlist = playlist_crate.GetComponent<Playlist>().songs;
         playlist_info.text = "";
         int i = 0;
 
         foreach (Song s in playlist)
         {
-            playlist_info.text += (i+1) + ". " + s.title + " by " + s.artist_name + "\n";
+            playlist_info.text += (i+1) + ". '" + s.title + "' by " + s.artist_name + "\n";
             i++;
         }
 	}
@@ -47,9 +51,16 @@ public class PlaylistBuilder : MonoBehaviour {
 		Song s = other.GetComponent<Song>();
 		if (s)
 		{
-			if (playlist.Add(s)) Debug.Log("Added " + s.title + " to playlist");
-			//else Debug.Log("Failed to add");
-			//other.gameObject.SetActive(false);
+			if (playlist.Add(s))
+			{
+				Debug.Log("Added " + s.title + " to playlist");
+
+				// unparent from selector
+				other.transform.SetParent(null);
+
+				// teleport cube to playlist crate
+				s.transform.position = playlist_crate.transform.position + new Vector3(0.0f, 0.5f, 0.0f);
+			}
 		}
 	}
 
